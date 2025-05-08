f0cc82456e38afe9d9075a57ccc9aa1644efd808
Pedro Lucas Ramos <pelucasrramos@gmail.com>
2025-05-08 10:36:45 -0300

    Add files via upload

---
+++ APIhealth/src/test/java/com/healthproject/APIhealth/ApIhealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthproject.APIhealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApIhealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
