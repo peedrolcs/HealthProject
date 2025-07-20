8f8a76e4690dc63e39a793a18e18ef10daebc50b
Pedro Lucas Ramos <pelucasrramos@gmail.com>
2025-07-19 22:57:18 -0300

    Add files via upload

---
+++ APIhealth/src/main/java/com/healthproject/APIhealth/ApIhealthApplication.java
@@ -0,0 +1,13 @@
+package com.healthproject.APIhealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApIhealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApIhealthApplication.class, args);
+	}
+
+}
